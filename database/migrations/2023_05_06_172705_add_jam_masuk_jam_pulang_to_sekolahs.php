1258c668c46e05054f9dde961921f50c1e4fd589
Adri-Bagas <abagaswitjaksono@gmail.com>
2023-05-06 17:30:26 +0700

    add column jam_masuk and jam_pulang to table sekolahs

---
+++ database/migrations/2023_05_06_172705_add_jam_masuk_jam_pulang_to_sekolahs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sekolahs', function (Blueprint $table) {
+            $table->time('jam_masuk')->nullable();
+            $table->time('jam_pulang')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sekolahs', function (Blueprint $table) {
+            $table->dropColumn('jam_pulang');
+            $table->dropColumn('jam_masuk');
+        });
+    }
+};
