dd3de9e05f73cd07e2a0e4ad4216c431b0679109
CodingRafi <codingmuhamadrafiprasetya@gmail.com>
2023-05-08 17:28:06 +0700

    source DB

--- database/seeders/RefProvinsiSeeder.php
+++ database/seeders/RefProvinsiSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 
 class RefProvinsiSeeder extends Seeder
 {
@@ -14,37 +15,9 @@ class RefProvinsiSeeder extends Seeder
      */
     public function run()
     {
-        $provinsis = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/provinsi.json'));
-
-        foreach ($provinsis as $key => $provinsi) {
-            DB::table('ref_provinsis')->insert([
-                'id' => $provinsi->id,
-                'nama' => $provinsi->nama,
-            ]);
-            $kabupatens = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kabupaten/'. $provinsi->id .'.json'));
-            foreach ($kabupatens as $key => $kabupaten) {
-                DB::table('ref_kabupatens')->insert([
-                    'id' => $kabupaten->id,
-                    'nama' => $kabupaten->nama,
-                    'ref_provinsi_id' => $provinsi->id
-                ]);
-                $kecamatans = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kecamatan/'. $kabupaten->id .'.json'));
-                foreach ($kecamatans as $key => $kecamatan) {
-                    DB::table('ref_kecamatans')->insert([
-                        'id' => $kecamatan->id,
-                        'nama' => $kecamatan->nama,
-                        'ref_kabupaten_id' => $kabupaten->id
-                    ]);
-                    $kelurahans = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kelurahan/'. $kecamatan->id .'.json'));
-                    foreach ($kelurahans as $key => $kelurahan) {
-                        DB::table('ref_kelurahans')->insert([
-                            'id' => $kelurahan->id,
-                            'nama' => $kelurahan->nama,
-                            'ref_kecamatan_id' => $kecamatan->id
-                        ]);
-                    }
-                }
-            }
-        }
+        DB::unprepared(file_get_contents(app_path(). "/db_wilayah/ref_provinsis.sql"));
+        DB::unprepared(file_get_contents(app_path(). "/db_wilayah/ref_kabupatens.sql"));
+        DB::unprepared(file_get_contents(app_path(). "/db_wilayah/ref_kecamatans.sql"));
+        DB::unprepared(file_get_contents(app_path(). "/db_wilayah/ref_kelurahans.sql"));
     }
 }
