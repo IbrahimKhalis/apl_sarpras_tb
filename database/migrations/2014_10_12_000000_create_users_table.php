c12e5954ce99b9bafe0ab8baa6cb44815c14e54d
ZeroneDoo <rizqipambudi6@gmail.com>
2023-05-10 15:50:19 +0700

    Update Admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('profil')->default('/img/profil.png');
+            $table->string('name');
+            $table->enum('jk', ['L', 'P'])->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('nip')->unique()->nullable();
-            $table->string('nipd')->unique()->nullable();
             $table->string('password')->default('$2a$12$5o5olF8lR9ySHktv1wpxQ.OHLmSLFXHt7ZYQxkN9QFfjJgE5v2bty');
             $table->foreignId('sekolah_id')->nullable()->constrained();
             $table->rememberToken();
