3fe2beb3dcfd2ff8946f011e6da24c7dcc4e1d4c
ibrahimkhalish <ibrahimkhalish@gmail.com>
2023-05-10 17:27:33 +0700

    slice navbar,topbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 @endpush
 
 @section('content')
-<div class="d-flex justify-content-between mb-3 align-items-center">
+{{-- <div class="d-flex justify-content-between mb-3 align-items-center">
     <h4><strong>Dashboard</strong></h4>
 </div>
 @if (Auth::user()->hasRole('super_admin'))
@@ -74,5 +74,46 @@
         </div>
     </div>
 </div>
-@endif
+@endif --}}
+
+<!-- BEGIN: General Report -->
+<div class="col-span-12 mt-8">
+    <div class="intro-y flex items-center h-10">
+        <h2 class="text-lg font-medium truncate mr-5">
+            Dashboard Report
+        </h2>
+        <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
+    </div>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+            <div class="report-box zoom-in">
+                <div class="box p-5">
+                    <div class="flex">
+                        <i data-lucide="monitor" class="report-box__icon text-warning"></i> 
+                        <div class="ml-auto">
+                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                        </div>
+                    </div>
+                    <div class="text-3xl font-medium leading-8 mt-6">{{ $countTahunAjaran }}</div>
+                    <div class="text-base text-slate-500 mt-1">Jumlah Tahun Ajaran</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+            <div class="report-box zoom-in">
+                <div class="box p-5">
+                    <div class="flex">
+                        <i data-lucide="user" class="report-box__icon text-success"></i> 
+                        <div class="ml-auto">
+                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                        </div>
+                    </div>
+                    <div class="text-3xl font-medium leading-8 mt-6">{{ $countRole }}</div>
+                    <div class="text-base text-slate-500 mt-1">Roles</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END: General Report -->
 @endsection
--- resources/views/mylayouts/main.blade.php
+++ resources/views/mylayouts/main.blade.php
@@ -9,12 +9,12 @@
 
   <link rel="stylesheet" href="{{ asset('css/fstdropdown.css') }}">
 
-  <title>Schoolio</title>
+  <title>Sarpras TB</title>
 
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('dist/css/app.css') }}" rel="stylesheet">
+  {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-  <style>
+  {{-- <style>
     .fstdropdown>.fstlist {
       min-height: 10rem !important;
     }
@@ -46,37 +46,55 @@
       color: #e9ecef;
     }
   </style>
-  @stack('css')
+  @stack('css') --}}
 </head>
 
-<body>
-  <div class="wrapper">
-    @include('mypartials.aside')
+<body class="py-5">
+  
+    @include('mypartials.mobile')
 
-    <div class="main">
-      @include('mypartials.navbar')
 
-      <main class="content">
-        @yield('content')
-      </main>
-
-      <footer class="footer">
-        <div class="container-fluid">
-          <div class="row text-muted justify-content-end">
-            <span>Proudly powered by <a href="https://github.com/CodingRafi">CodingRafi</a></span>
+    <div class="flex">
+      @include('mypartials.aside')
+      <div class="content">
+        @include('mypartials.navbar')
+        <div class="grid grid-cols-12 gap-6">
+          <div class="col-span-12 2xl:col-span-9">
+            <div class="grid grid-cols-12 gap-6">
+              @yield('content')
+            </div>
           </div>
         </div>
-      </footer>
+       
+      </div>
     </div>
-  </div>
+  
+      
+
+     
+       
+      
+
+      
+   
 
   <form action="" class="form-delete" method="POST">
     @csrf
     @method('delete')
     @stack('other_delete')
   </form>
+<script>
+  const sideMenuLinks = document.querySelectorAll('.side-menu');
 
-  <script src="{{ asset('js/app.js') }}"></script>
+sideMenuLinks.forEach(link => {
+    // cek apakah URL halaman saat ini sama dengan tautan menu
+    if (link.href === window.location.href) {
+        // tambahkan kelas "aktif" ke tautan menu
+        link.classList.add('side-menu--active');
+        }})
+</script>
+  {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+  <script src="{{ asset('dist/js/app.js') }}"></script>
   @include('mypartials.js')
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="{{ asset('js/fstdropdown.js') }}"></script>
