44c9eab50b6b6db22c4b808e291404844981ef85
[Kiagusss] <[kiagus.muslim78@gmail.com]>
2023-05-25 16:18:36 +0700

    Input image bisa, tapi penggunaan dari fancybox masih belum bisa

--- resources/views/mylayouts/main.blade.php
+++ resources/views/mylayouts/main.blade.php
@@ -6,11 +6,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
-
+   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+  <link href="https://unpkg.com/tailwindcss@1.2.0/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/fstdropdown.css') }}">
 
   <title>Sarpras TB</title>
-
+ 
+  <link
+  rel="stylesheet"
+  href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css"type="text/css"/>
   <link href="{{ asset('dist/css/app.css') }}" rel="stylesheet">
   {{--
   <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
@@ -96,6 +102,14 @@
   {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
   <script src="{{ asset('dist/js/app.js') }}"></script>
   @include('mypartials.js')
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+  <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+  <script src="path/to/vanilla.js"></script>                 
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+  <script src="https://unpkg.com/create-file-list"></script>
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="{{ asset('js/fstdropdown.js') }}"></script>
   <script>
