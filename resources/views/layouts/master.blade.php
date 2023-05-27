310eb85898b951ccbcfb0f5b5e58bf0d57d77555
[Kiagusss] <[kiagus.muslim78@gmail.com]>
2023-05-27 22:45:09 +0700

    revisi dikit landing page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,13 +11,15 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://unpkg.com/tailwindcss@1.2.0/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
+    <script defer src="https://unpkg.com/alpinejs@3.2.3/dist/cdn.min.js"></script>
     <link href="{{ asset('dist/css/app.css') }}" rel="stylesheet">
     @yield('css')
 </head>
 
-<body>
+<body class="">
     @include('components.nav')
 
     @yield('content')
