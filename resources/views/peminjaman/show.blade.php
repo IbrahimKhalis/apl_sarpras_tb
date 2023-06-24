fa1f94a04bae8fae8a3ec350273df1a5f1011741
CodingRafi <codingmuhamadrafiprasetya@gmail.com>
2023-06-24 19:41:46 +0700

    detail peminjaman

--- resources/views/peminjaman/show.blade.php
+++ resources/views/peminjaman/show.blade.php
@@ -1,13 +1,15 @@
 @extends('myLayouts.main')
 
 @section('content')
-<div class="intro-y flex flex-col sm:flex-row items-center mt-8">
-    <h2 class="text-lg font-Medium mr-auto">
-        Peminjaman
-    </h2>
-    @can('edit_peminjaman')
-    <a href="{{ route('peminjamans.edit', $data->id) }}">Edit</a>
-    @endcan
+<div class="intro-y box">
+    <div class="flex flex-col sm:flex-row items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400">
+        <h2 class="font-medium text-base mr-auto">
+            Peminjaman
+        </h2>
+        @can('edit_peminjaman')
+        <a href="{{ route('peminjamans.edit', $data->id) }}" class="btn btn-sm btn-warning">Edit</a>
+        @endcan
+    </div>
+    
 </div>
-
 @endsection
